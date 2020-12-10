31ab6a9b3c8e62469d4eb298bccc380da30ed329
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-12-10 13:18:03 -0500

    edit script/style(hover state edits)
    add dropdown nav hover states/footer social icon hover state imgs/footer social mouseenter & mouseleave events

--- script.js
+++ script.js
@@ -50,4 +50,43 @@ document.addEventListener("DOMContentLoaded", function() {
     this.style.outlineWidth = "initial";
   });
 
+/* ----- */
+  document.getElementById("facebook-link").addEventListener("mouseenter", function() {
+    this.children[0].src = "imgs/icon-facebook-cyan.svg";
+  });
+
+  document.getElementById("facebook-link").addEventListener("mouseleave", function() {
+    this.children[0].src = "imgs/icon-facebook-white.svg";
+  });
+
+// --
+
+  document.getElementById("twitter-link").addEventListener("mouseenter", function() {
+    this.children[0].src = "imgs/icon-twitter-cyan.svg";
+  });
+
+  document.getElementById("twitter-link").addEventListener("mouseleave", function() {
+    this.children[0].src = "imgs/icon-twitter-white.svg";
+  });
+
+// --
+
+  document.getElementById("pinterest-link").addEventListener("mouseenter", function() {
+    this.children[0].src = "imgs/icon-pinterest-cyan.svg";
+  });
+
+  document.getElementById("pinterest-link").addEventListener("mouseleave", function() {
+    this.children[0].src = "imgs/icon-pinterest-white.svg";
+  });
+
+// --
+
+  document.getElementById("instagram-link").addEventListener("mouseenter", function() {
+    this.children[0].src = "imgs/icon-instagram-cyan.svg";
+  });
+
+  document.getElementById("instagram-link").addEventListener("mouseleave", function() {
+    this.children[0].src = "imgs/icon-instagram-white.svg";
+  });
+
 });
